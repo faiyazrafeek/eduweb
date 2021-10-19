aa3aa6afad6fc6888ba1df471176e3275656b3ba
Faiyaz Rafeek <faiyazah_ct16014@stu.kln.ac.lk>
2021-10-19 12:19:02 +0530

    cloud db config

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "eduweb";
+$servername = "remotemysql.com";
+$username = "f0ybxrlmLO";
+$password = "Bx0WGyvFt6";
+$dbname = "f0ybxrlmLO";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
